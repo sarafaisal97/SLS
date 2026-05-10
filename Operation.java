793c9506f5654ca393fb92a635997b7a6a479bf3
Leen20031 <Leen_170@hotmail.com>
2026-05-10 10:13:48 +0300

    Add multiply and divide methods to Operations class

--- Operation.java
+++ Operation.java
@@ -1,2 +1,10 @@
 public class Operations {
 }
+public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) return 0;
+        return (double) a / b;
+    }
